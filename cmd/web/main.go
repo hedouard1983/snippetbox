ca8f2246653d4968abc6b4d8b7a5d4a1bbe5eb4a
Henry Edouard <h.edouard@gmail.com>
2020-11-26 17:52:30 -0600

    UI

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,13 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
